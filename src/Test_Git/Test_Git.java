b198aaaad5c01df65140f2b5a761b91d2d76f63c
siva <siva@manjunathan-junior-qa>
2022-10-05 17:48:36 +0530

    adding sysout scenerio

--- src/Test_Git/Test_Git.java
+++ src/Test_Git/Test_Git.java
@@ -5,12 +5,16 @@ public class Test_Git {
 	public static void main(String[] args) {
 
 		System.out.println("Test");
+		
 		System.out.println("Test");
 		System.out.println("Test");
 		System.out.println("Test");
 		System.out.println("Test");
 		System.out.println("Test");
 		System.out.println("Test");
+		System.out.println("Test");
+		
+		
 
 	}
 
