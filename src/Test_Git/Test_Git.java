dd7164fe8188a421988fd679f45d5aa5dcdbbb5b
siva <siva@manjunathan-junior-qa>
2022-10-05 13:30:29 +0530

    Add feature

--- src/Test_Git/Test_Git.java
+++ src/Test_Git/Test_Git.java
@@ -5,6 +5,12 @@ public class Test_Git {
 	public static void main(String[] args) {
 
 		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
 
 	}
 
